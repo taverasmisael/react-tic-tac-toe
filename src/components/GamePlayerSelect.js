5d5d95bf0db330724f6dbe3ee60892b8a5ae705c
TaverasMisael <macrot29@gmail.com>
2017-03-11 20:02:02 -0400

    feat: create GamePlayerSelect to selecting player1
    Everytime a new Match is started we let the user pick what will be its MainPlayer

---
+++ src/components/GamePlayerSelect.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "./GamePlayerSelect.css";
+
+export const GamePlayerSelect = ({ onPlayerSelect }) => (
+  <div className="GamePlayerSelect">
+    <div className="GamePlayerSelect__container">
+      <h1>Elegir al jugador 1</h1>
+      <span className="flex--spandex" />
+      <div className="GamePlayerSelect__selector">
+        <button
+          className="btn btn--big GamePlayerSelect__panel"
+          onClick={() => onPlayerSelect("X")}
+        >
+          X
+        </button>
+        <button
+          className="btn btn--big GamePlayerSelect__panel"
+          onClick={() => onPlayerSelect("O")}
+        >
+          O
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+GamePlayerSelect.propTypes = {
+  onPlayerSelect: React.PropTypes.func.isRequired
+};
+
+export default GamePlayerSelect;
