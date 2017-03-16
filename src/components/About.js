5d1ca963345dd77d31d500119a9a9d789757c577
TaverasMisael <macrot29@gmail.com>
2017-03-16 12:20:51 -0400

    feat: add basic styles and markup to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import './About.css'
+import React from 'react';
+import './About.css';
 
-export const AboutModal = ({isVisible, onClose}) => (
+export const AboutModal = ({ isVisible, onClose }) => (
   <div className={`AboutModal ${isVisible ? 'AboutModal--is-visible' : ''}`}>
-    <h2>¿Cómo jugar?</h2>
-    <div className="AboutModal__instructions">
-      Instrucciones
+    <h1 className="AboutModal__title">Acerca de</h1>
+    <button
+      className="btn btn--transparent btn--circle AboutModal__btn-close"
+      type="button"
+      onClick={() => onClose()}
+    >
+      X
+    </button>
+    <div className="AboutModal__content">
+      <div className="AboutModal__content__block">
+        <h2>¿Cómo jugar?</h2>
+        <div className="AboutModal__instructions">
+          Instrucciones
+        </div>
+      </div>
+      <div className="AboutModal__content__block">
+        <h2>Sobre los Programadores</h2>
+        <div className="AboutModal__instructions">
+          Nombres
+        </div>
+      </div>
     </div>
   </div>
-)
+);
 
 export default AboutModal;
 
 AboutModal.propTypes = {
   isVisible: React.PropTypes.bool.isRequired,
   onClose: React.PropTypes.func.isRequired
-}
+};
 
 AboutModal.defaultProps = {
   isVisible: false
