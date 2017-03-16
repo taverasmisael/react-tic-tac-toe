8639a4db050f358a93cc9a1ba1d2f631ee43395e
TaverasMisael <macrot29@gmail.com>
2017-03-16 14:20:40 -0400

    feat: complete AboutModel Styles and content

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -13,15 +13,44 @@ export const AboutModal = ({ isVisible, onClose }) => (
     </button>
     <div className="AboutModal__content">
       <div className="AboutModal__content__block">
-        <h2>¿Cómo jugar?</h2>
+        <h2 className="AboutModal__content_title">¿Cómo jugar?</h2>
         <div className="AboutModal__instructions">
-          Instrucciones
+          <p>
+            Es un juego entre dos jugadores: <em>X</em> y <em>O</em>, que marcan los espacios de un tablero de 3×3 alternadamente.
+            Un jugador gana si consigue tener una línea de tres de sus símbolos: <strong>la línea puede ser horizontal, vertical o diagonal.</strong>
+          </p>
+          <p>Elige el simbolo de jugador 1, y empieza una partida. ¡Suerte!</p>
         </div>
       </div>
       <div className="AboutModal__content__block">
-        <h2>Sobre los Programadores</h2>
+        <h2 className="AboutModal__content_title">Desarrolladores</h2>
         <div className="AboutModal__instructions">
-          Nombres
+          <table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Matrícula</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>MisaelTaveras</td>
+                <td>13-EIST-1-017</td>
+              </tr>
+              <tr>
+                <td>Elvis Vanderpool</td>
+                <td>13-EIST-1-058</td>
+              </tr>
+              <tr>
+                <td>Odani Eustaqui</td>
+                <td>13-MISM-1-075</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
