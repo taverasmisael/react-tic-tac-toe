8d7d030c9165b310c5877d8691b1c14369cf8c79
TaverasMisael <macrot29@gmail.com>
2017-03-24 10:50:45 -0400

    feat: markup placeholders for GameConfigBar blocks

--- src/components/GameConfigBar.js
+++ src/components/GameConfigBar.js
@@ -4,13 +4,21 @@ import './GameConfigBar.css';
 
 const GameConfigBar = ({currentPlayer, onResetGame, winner}) => (
   <header className="GameConfigBar">
-    <div className="GameConfigBar__current-player">
-      <span className="GameConfigBar__cururent-player__label">{ winner  ? 'El ganador es' : 'Es el turno de' }:</span>
+    <div className="config-block current-player">
+      <span className="config-block__header">{ winner  ? 'El ganador es' : 'Es el turno de' }:</span>
       <span className={computeCurrentPlayerClasses(winner)}>{ currentPlayer }</span>
     </div>
-    <div className="GameConfigBar__timer">
-      <span>Tiempo</span>
-      <span>{new Date().getUTCDate()}</span>
+    <div className="config-block color-picker">
+      <span className="config-block__header">Configuración de Sonido</span>
+      <span className="config-block__content">{new Date().getUTCDate()}</span>
+    </div>
+    <div className="config-block timer">
+      <span className="config-block__header">Tiempo</span>
+      <span className="config-block__content">{new Date().getUTCDate()}</span>
+    </div>
+    <div className="config-block color-picker">
+      <span className="config-block__header">Color de Fondo</span>
+      <span className="config-block__content">{new Date().getUTCDate()}</span>
     </div>
     <button className="btn btn--big btn--warn" onClick={() => onResetGame()}>{winner ? 'Nueva Partida' : 'Reiniciar Partida'}</button>
   </header>
@@ -21,8 +29,9 @@ GameConfigBar.propTypes = {
   winner: React.PropTypes.bool.isRequired,
 }
 
+export default GameConfigBar;
+
 function computeCurrentPlayerClasses(winner) {
-  const baseClases = 'GameConfigBar__current-player__player';
+  const baseClases = 'config-block__content current-player__player';
   return winner ? `${baseClases} text--accent` : baseClases;
 }
-export default GameConfigBar;
