1b835f213bf5d412130ebb4beae1aac152b84436
TaverasMisael <macrot29@gmail.com>
2017-03-25 11:05:03 -0400

    feat: add function to generate classes dynamically

--- src/functionality/helpers.js
+++ src/functionality/helpers.js
@@ -1,3 +1,13 @@
 export const eq = (obj1, obj2) => obj1 === obj2;
-export const identity = (el) => el;
-export const notIdentity = (el) => !identity(el);
+export const identity = el => el;
+export const notIdentity = el => !identity(el);
+
+/**
+ *
+ * @param {String} base This Class ALWAYS will be applied to the element/component
+ * @param {Array<String>} extra All the other classes we want to apply to the element if the predicate is true
+ * @param {Boolean} predicate This condition determinates if the extra classes are aplied or not
+ * @returns {String} All the classes the element need based on the @param predicate
+ */
+export const dynamicClass = (base, extra, predicate) =>
+  predicate ? `${base} ${extra.join(' ')}` : base;
