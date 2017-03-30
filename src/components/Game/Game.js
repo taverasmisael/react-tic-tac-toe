7c89daf4fe182f75529728a2fbfae2d92fc59e7a
TaverasMisael <macrot29@gmail.com>
2017-03-29 23:44:47 -0400

    feat: implement basic markup for GameStadistics

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,21 +1,24 @@
 import React from "react";
 
 import GameBoard from "./GameBoard";
+import GameStadistics from "../ui/GameStadistics";
 import "./Game.css";
 
-export const Game = ({winner, board, onSelectSquare}) => (
+export const Game = ({winner, board, onSelectSquare, history}) => (
   <div className="Game">
     <GameBoard
       board={board}
       winner={winner}
       onSelectSquare={(square) => onSelectSquare(square)}
     />
+    <GameStadistics states={history} />
   </div>
 );
 
 Game.propTypes = {
   winner: React.PropTypes.array,
   board: React.PropTypes.array.isRequired,
+  history: React.PropTypes.array.isRequired,
   onSelectSquare: React.PropTypes.func.isRequired,
 }
 
---
+++ src/components/ui/GameStadistics.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import './GameStadistics.css';
+
+export const GameStadistics = ({states}) => (
+  <div className="GameStadistics">
+    <table>
+    <thead>
+      <tr>
+        <th>Partida</th>
+        <th>Ganador</th>
+        <th>Tiempo</th>
+        <th>Puntaje</th>
+      </tr>
+    </thead>
+    <tbody>
+      {generateTable(states)}
+    </tbody>
+  </table>
+  </div>
+);
+
+export default GameStadistics;
+
+GameStadistics.propTypes = {
+  states: React.PropTypes.array.isRequired
+}
+GameStadistics.defaultProps = {
+  states: []
+}
+
+/**
+ *
+ * @param {Array<Object>} states
+ */
+function generateTable(states) {
+  return states.map((state, index) => (
+    <tr>
+      <td>{index}</td>
+      <td>{state.winner}</td>
+      <td>{state.time}</td>
+      <td>{state.score}</td>
+    </tr>
+  ))
+}
