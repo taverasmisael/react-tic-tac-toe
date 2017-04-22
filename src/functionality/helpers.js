9ee7c16dae7dac82698bd2b46835e84079feb655
TaverasMisael <macrot29@gmail.com>
2017-04-21 23:49:40 -0400

    feat: add support for extra as String|Array<string>

--- src/functionality/helpers.js
+++ src/functionality/helpers.js
@@ -5,12 +5,12 @@ export const notIdentity = el => !identity(el);
 /**
  *
  * @param {String} base This Class ALWAYS will be applied to the element/component
- * @param {Array<String>} extra All the other classes we want to apply to the element if the predicate is true
+ * @param {String|Array<String>} extra All the other classes we want to apply to the element if the predicate is true
  * @param {Boolean} predicate This condition determinates if the extra classes are aplied or not
  * @returns {String} All the classes the element need based on the @param predicate
  */
 export const dynamicClass = (base, extra, predicate) =>
-  predicate ? `${base} ${extra.join(' ')}` : base;
+  predicate ? `${base} ${Array.isArray(extra) ? extra.join(' ') : extra}` : base;
 
 /**
  * This function increments by one the passed value
