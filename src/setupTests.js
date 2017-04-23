94f318f3c1646728eebe4cd692970d8307ec1ebf
TaverasMisael <macrot29@gmail.com>
2017-04-23 11:57:20 -0400

    style: format setupTest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,11 @@
 const mock = () => {
-    let storage = {};
-    return {
-        getItem: key => key in storage ? storage[key] : null,
-        setItem: (key, value) => storage[key] = value || '',
-        removeItem: key => delete storage[key],
-        clear: () => storage = {},
-    };
+  let storage = {};
+  return {
+    getItem: key => (key in storage ? storage[key] : null),
+    setItem: (key, value) => (storage[key] = value || ''),
+    removeItem: key => delete storage[key],
+    clear: () => (storage = {})
+  };
 };
 
 Object.defineProperty(window, 'localStorage', { value: mock() });
