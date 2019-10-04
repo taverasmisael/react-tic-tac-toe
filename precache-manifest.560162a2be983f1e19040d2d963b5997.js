3f599b630fe0fbbd9770bd30a3f9964ea0760ed1
Misael Taveras <macrot29@gmail.com>
2019-10-04 11:48:58 -0400

    Updates

---
+++ precache-manifest.560162a2be983f1e19040d2d963b5997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b349222380f17e6311101424cbee9e",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "617750fb9b38cf78e9bf",
+    "url": "/react-tic-tac-toe/static/css/main.d00bfd6f.chunk.css"
+  },
+  {
+    "revision": "134e61f599f171ee4ae9",
+    "url": "/react-tic-tac-toe/static/js/2.656d0094.chunk.js"
+  },
+  {
+    "revision": "617750fb9b38cf78e9bf",
+    "url": "/react-tic-tac-toe/static/js/main.c3c9ba6b.chunk.js"
+  },
+  {
+    "revision": "f397ea6ba4e37f5a3e40",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.ffff6b86.js"
+  }
+]);
