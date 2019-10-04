05a66df7ac06b402526bb29aa8c1047309e47f77
Misael Taveras <macrot29@gmail.com>
2019-10-04 11:21:39 -0400

    fix: history sort by number in score or time

--- src/components/ui/GameStadistics.js
+++ src/components/ui/GameStadistics.js
@@ -1,39 +1,43 @@
-import React from 'react';
+import React from 'react'
 import PropTypes from 'prop-types'
 
-import './GameStadistics.css';
+import './GameStadistics.css'
 
-export const GameStadistics = ({states, onResetScores}) => (
+export const GameStadistics = ({ states, onResetScores }) => (
   <div className="GameStadistics">
     <table>
-    <thead>
-      <tr>
-        <th>Partida</th>
-        <th>Ganador</th>
-        <th>Tiempo <small>(segs)</small></th>
-        <th>Puntaje</th>
-        <th>
-          <button className="btn btn--circle btn--warn" title="Borrar Puntajes" onClick={() => onResetScores()}>
-            <i className="material-icons">delete_sweep</i>
-          </button>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      {generateTable(states)}
-    </tbody>
-  </table>
+      <thead>
+        <tr>
+          <th>Pos.</th>
+          <th>Ganador</th>
+          <th>
+            Tiempo <small>(segs)</small>
+          </th>
+          <th>Puntaje</th>
+          <th>
+            <button
+              className="btn btn--circle btn--warn"
+              title="Borrar Puntajes"
+              onClick={() => onResetScores()}
+            >
+              <i className="material-icons">delete_sweep</i>
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>{generateTable(states)}</tbody>
+    </table>
   </div>
-);
+)
 
-export default GameStadistics;
+export default GameStadistics
 
 GameStadistics.propTypes = {
   states: PropTypes.array.isRequired,
-  onResetScores: PropTypes.func.isRequired
+  onResetScores: PropTypes.func.isRequired,
 }
 GameStadistics.defaultProps = {
-  states: []
+  states: [],
 }
 
 /**
